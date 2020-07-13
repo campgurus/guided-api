b1d2c987e63628e737bb653b40683319e1acb36b
Darius Goore <dariusgoore@protonmail.com>
2020-07-13 19:45:33 -0400

    adding knex config

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: "mysql",
+    connection: "mysql://root:123456@localhost:3306/guided_dev",
+    migrations: {
+      directory: __dirname + "/knex/migrations",
+    },
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
